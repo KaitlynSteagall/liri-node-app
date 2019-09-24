f22bfa1768a9fdc795c5cbb9cf10f286b67afc00
Kaitlyn Steagall <kaitlynsteagall@gmail.com>
2019-09-24 13:10:30 -0700

    adjust spacing

--- liri.js
+++ liri.js
@@ -26,30 +26,61 @@ case "do-what-it-says":
 
 function getMovie() {
   
-var movie = "";
-for (var i = 3; i < addtlWords.length; i++) {
-  if (i > 3 && i < addtlWords.length) {
-    movie = movie + "+" + addtlWords[i];
+  var movie = "";
+  
+  for (var i = 3; i < addtlWords.length; i++) {
+    if (i > 3 && i < addtlWords.length) {
+      movie = movie + "+" + addtlWords[i];
+    }
+    else {
+      movie += addtlWords[i];
+    }
   }
+
+  var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+
+  if (movie) {
+    axios.get(queryUrl).then(
+      function(response) {
+        console.log("Movie: " + response.data.Title);
+        console.log("Release Year: " + response.data.Year);
+        console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+        console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+        console.log("Country where produced: " + response.data.Country);
+        console.log("Language in: " + response.data.Language);
+        console.log("Plot: " + response.data.Plot);
+        console.log("Actors: " + response.data.Actors);
+      }
+    )
+    .catch(function(error) {
+      if (error.response) {
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Headers---------------");
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    })
+  } 
   else {
-    movie += addtlWords[i];
-  }
-}
-
-var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-
-if (movie){
-  axios.get(queryUrl).then(
-    function(response) {
-      console.log("Movie: " + response.data.Title);
-      console.log("Release Year: " + response.data.Year);
-      console.log("IMDB Rating: " + response.data.Ratings[0].Value);
-      console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-      console.log("Country where produced: " + response.data.Country);
-      console.log("Language in: " + response.data.Language);
-      console.log("Plot: " + response.data.Plot);
-      console.log("Actors: " + response.data.Actors);
-    })
+    axios.get("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy").then(
+      function(response) {
+        console.log("Movie: " + response.data.Title);
+        console.log("Release Year: " + response.data.Year);
+        console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+        console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+        console.log("Country where produced: " + response.data.Country);
+        console.log("Language in: " + response.data.Language);
+        console.log("Plot: " + response.data.Plot);
+        console.log("Actors: " + response.data.Actors);
+      }
+    )
     .catch(function(error) {
       if (error.response) {
         console.log("---------------Data---------------");
@@ -66,32 +97,4 @@ if (movie){
       console.log(error.config);
     })
   }
-else {
-  axios.get("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy").then(
-    function(response) {
-      console.log("Movie: " + response.data.Title);
-      console.log("Release Year: " + response.data.Year);
-      console.log("IMDB Rating: " + response.data.Ratings[0].Value);
-      console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-      console.log("Country where produced: " + response.data.Country);
-      console.log("Language in: " + response.data.Language);
-      console.log("Plot: " + response.data.Plot);
-      console.log("Actors: " + response.data.Actors);
-    })
-    .catch(function(error) {
-      if (error.response) {
-        console.log("---------------Data---------------");
-        console.log(error.response.data);
-        console.log("---------------Status---------------");
-        console.log(error.response.status);
-        console.log("---------------Headers---------------");
-        console.log(error.response.headers);
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log("Error", error.message);
-      }
-      console.log(error.config);
-    });
-  }
-}
+};
